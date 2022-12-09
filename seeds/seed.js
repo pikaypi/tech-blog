a2faec160710668123bd9ba11b28fe99041832af
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-09 12:09:08 -0500

    collect seeds and seed database

---
+++ seeds/seed.js
@@ -0,0 +1,16 @@
+const sequelize = require('../config/connection');
+const seedUsers = require('./userData');
+const seedPosts = require('./postData');
+const seedComments = require('./commentData');
+
+const seed = async () => {
+    await sequelize.sync({ force: true });
+
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+
+    process.exit(0);
+};
+
+seed();
