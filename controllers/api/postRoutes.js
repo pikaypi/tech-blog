98a5b8d2027449ef36c3fd3aaa0202b63f1916e1
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-09 13:16:25 -0500

    api routes to get all posts or a single post

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { Comment, Post, User } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({ include: { model: Comment }});
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, { include: { model: Comment }});
+        postData ? res.status(200).json(postData) : res.status(404).json({ message: 'Post not found'})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
