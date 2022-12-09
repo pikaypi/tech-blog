636807b0d882d6eace885cacf4c515c1812e63bf
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-09 15:12:49 -0500

    GET route to render homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const { Post } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-        console.log(`${req.method} request received for homepage`);
+        const postData = await Post.findAll();
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', { posts });
     } catch (err) {
         res.status(500).json(err);
     }
