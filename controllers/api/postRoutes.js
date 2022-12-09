89f8bbb36cd293e985b9ceadd6c1b6e6c54ee99e
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-09 13:37:22 -0500

    POST route to create new Post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -19,4 +19,13 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create(req.body);
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
