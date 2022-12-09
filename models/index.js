4b9f27940879f4a7fbe923acca26bfd1bb86a2ff
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-09 11:51:02 -0500

    define model relations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,29 @@
-const User = require('./User');
+const Comment = require('./Comment');
 const Post = require('./Post');
+const User = require('./User');
 
-module.exports = { Post, User };
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { Comment, Post, User };
