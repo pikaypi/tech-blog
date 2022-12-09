54c0797b58272016571f8bbeaf09d33389b893ee
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-09 17:04:22 -0500

    add logged_in token to existing routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
 
         const posts = postData.map((post) => post.get({ plain: true }));
 
-        res.render('homepage', { posts });
+        res.render('homepage', { posts, logged_in: req.session.logged_in });
     } catch (err) {
         res.status(500).json(err);
     }
@@ -36,7 +36,7 @@ router.get('/posts/:id', async (req, res) => {
         });
         
         const post = postData.get({ plain: true });
-        res.render('post', { post });
+        res.render('post', { ...post, logged_in: req.session.logged_in });
     } catch (err) {
         res.status(500).json(err);
     }
