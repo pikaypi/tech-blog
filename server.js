43c534abf3fdadeb0f8a5725b9dc35068133442d
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-09 15:12:03 -0500

    add handlebars engine to server

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
