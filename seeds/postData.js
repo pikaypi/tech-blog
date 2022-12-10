38fda52473a86bd324e4a095caea2f414837f3fd
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-10 11:43:20 -0500

    edit seeds so all users have a post

--- seeds/postData.js
+++ seeds/postData.js
@@ -7,14 +7,19 @@ const postData = [
         "text": "This is the first post on this site! Isn't that exciting?"
     },
     {
-        "user_id": 3,
+        "user_id": 2,
         "title": "I just noticed",
         "text": "Does anyone else love flying, or is it just me?"
     },
     {
-        "user_id": 4,
+        "user_id": 3,
         "title": "Urban Canyons",
         "text": "Swing, swing, swing from the tangles of my wrists..."
+    },
+    {
+        "user_id": 4,
+        "title": "Suggestions?",
+        "text": "What other features would people like to see on here?"
     }
 ];
 
