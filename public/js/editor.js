ac80bd5442feb95e49ffe37f6487127aa1dcc34f
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-10 15:33:31 -0500

    store permanent dynamic elements in variables

---
+++ public/js/editor.js
@@ -0,0 +1,6 @@
+const titleEl = document.getElementById('title');
+const messageEl = document.getElementById('message');
+const postTypeEl = document.getElementById('post-type');
+const formEl = document.getElementById('message-form');
+const buttonGroupEl = document.getElementById('editor-button-group');
+const postListEl = document.getElementById('post-container');
