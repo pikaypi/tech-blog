ed34a7732c44ce77054fa12d4a57145dd9d3b276
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-10 11:44:02 -0500

    PUT route to edit posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Comment, Post, User } = require('../../models');
 
+// Dev only route to view all posts
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({ 
@@ -26,6 +27,8 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET route to return the contents of a single post
+// This route is used to populate the Post Editor in the dashboard
 router.get('/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, { 
@@ -51,12 +54,23 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+// POST route to create a new post
 router.post('/', async (req, res) => {
     try {
         const newPost = await Post.create(req.body);
         res.status(200).json(newPost);
     } catch (err) {
         res.status(500).json(err);
+    }
+})
+
+// PUT route to edit an existing post
+router.put('/:id', async (req, res) => {
+    try {
+        const editPost = await Post.update(req.body, { where: { id: req.params.id }});
+        res.status(200).json(editPost);
+    } catch (err) {
+        res.status(500).json(err)
     }
 })
 
