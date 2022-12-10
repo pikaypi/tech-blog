9b68a313b71f6e66f17c6277c0bf05cd22b59a54
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-10 15:35:48 -0500

    function that renders the form to edit posts

--- public/js/editor.js
+++ public/js/editor.js
@@ -33,3 +33,48 @@ const renderNewPostForm = () => {
     formEl.removeEventListener('submit', submitEditHandler);
     formEl.addEventListener('submit', submitNewHandler);
 }
+
+// Creates and renders the buttons for editing an existing post
+const renderEditPostForm = (post_id) => {
+    while (buttonGroupEl.firstChild) {
+        buttonGroupEl.removeChild(buttonGroupEl.firstChild);
+    }
+
+    // Set the post-type header
+    postTypeEl.innerHTML = 'Edit Post';
+    postTypeEl.setAttribute('style', 'color: #eaaa00');
+
+    // Create submit-edit button
+    const submitEditBtn = document.createElement('button');
+    submitEditBtn.setAttribute('id', 'submit-edit-btn');
+    submitEditBtn.classList.add('btn', 'submit-edit-btn');
+    submitEditBtn.innerHTML = 'submit';
+
+    // Create cancel button
+    const cancelBtn = document.createElement('button');
+    cancelBtn.setAttribute('id', 'cancel-btn');
+    cancelBtn.classList.add('btn', 'cancel-btn');
+    cancelBtn.innerHTML = 'cancel';
+
+    // Create delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('id', 'delete-btn');
+    deleteBtn.setAttribute('data-post-id', `${post_id}`)
+    deleteBtn.classList.add('btn', 'delete-btn');
+    deleteBtn.innerHTML = 'delete'
+
+    // Append the new buttons to the button group
+    buttonGroupEl.append(submitEditBtn);
+    buttonGroupEl.append(cancelBtn);
+    buttonGroupEl.append(deleteBtn);
+
+    // Replace submit new post event listener with submit edit post event listener
+    // and add post id to the form element dataset
+    formEl.removeEventListener('submit', submitNewHandler);
+    formEl.addEventListener('submit', submitEditHandler);
+    formEl.setAttribute('data-post-id', `${post_id}`);
+
+    // Add cancel and delete button event listeners
+    cancelBtn.addEventListener('click', cancelButtonHandler);
+    deleteBtn.addEventListener('click', deletePostHandler);
+}
